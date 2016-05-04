fc2a8153e11b743be976d69e29e988fb8ba9c5ba
Chu Xiaonan <chuxiaonan@gmail.com>
2016-05-04 17:28:22 +0800

    update hlpl.user.js
    修改一些css类名称和js函数名，使它们保持一致

--- hlpl.user.js
+++ hlpl.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         hlpl.user.js
 // @namespace    https://raw.githubusercontent.com/chuxiaonan/Highlight-promotion-links-of-Baidu-search-results/master/hlpl.user.js
-// @version      0.4
+// @version      0.41
 // @description  Highlight promotion links of Baidu search results
 // @author       Chu Xiaonan
 // @match        https://www.baidu.com/*
@@ -11,16 +11,16 @@
 (function() {
     'use strict';
 
-    var css = '.btn{display:block;position:relative;border:double 8px red;-webkit-transition:width .8s cubic-bezier(0.23,1,0.32,1),height .8s cubic-bezier(0.23,1,0.32,1),-webkit-transform .8s cubic-bezier(0.175,0.885,0.32,1.275);transition:width .8s cubic-bezier(0.23,1,0.32,1),height .8s cubic-bezier(0.23,1,0.32,1),transform .8s cubic-bezier(0.175,0.885,0.32,1.275);-webkit-transform-style:preserve-3d;transform-style:preserve-3d;-webkit-transform-origin:50% 50%;-ms-transform-origin:50% 50%;transform-origin:50% 50%}.btn-front{z-index:999;position:absolute;display:block;background-color:#fff;width:100%;height:100%;-webkit-backface-visibility:hidden;backface-visibility:hidden;-webkit-tap-highlight-color:transparent;-webkit-transition:background .15s ease,line-height .8s cubic-bezier(0.23,1,0.32,1);transition:background .15s ease,line-height .8s cubic-bezier(0.23,1,0.32,1)}.btn-front:hover{background-color:#f77066}.not-allowed .btn-front{cursor:pointer}.btn-back{z-index:0;position:absolute;width:100%;height:100%;background-color:#eee;color:#222;-webkit-transform:translateZ(-2px) rotateX(180deg);transform:translateZ(-2px) rotateX(180deg);overflow:hidden;-webkit-transition:box-shadow .8s ease;transition:box-shadow .8s ease;text-align:center}.btn-back p{margin-top:15px;margin-bottom:10px}.btn-back button{padding:12px 20px;width:30%;margin:0 5px;background-color:transparent;border:0;border-radius:2px;font-size:1em;cursor:pointer;-webkit-appearance:none;-webkit-tap-highlight-color:transparent;-webkit-transition:background .15s ease;transition:background .15s ease}.btn-back button:focus{outline:0}.btn-back button.yes{background-color:#2196f3;color:#fff}.btn-back button.yes:hover{background-color:red}.btn-back button.no{color:#2196f3}.btn-back button.no:hover{background-color:#ddd}.btn[data-direction="left"] .btn-back,.btn[data-direction="right"] .btn-back{-webkit-transform:translateZ(-2px) rotateY(180deg);transform:translateZ(-2px) rotateY(180deg)}.btn.is-open{border:solid 8px #eee}.btn.is-open .btn-front{pointer-events:none;z-index:0}.btn.is-open .btn-back{z-index:999}.btn[data-direction="top"].is-open{-webkit-transform:rotateX(180deg);transform:rotateX(180deg);box-shadow:0 -6px 8px rgba(0,0,0,0.4)}.btn[data-direction="right"].is-open{-webkit-transform:rotateY(180deg);transform:rotateY(180deg);box-shadow:0 6px 8px rgba(0,0,0,0.4)}.btn[data-direction="bottom"].is-open{-webkit-transform:rotateX(-180deg);transform:rotateX(-180deg);box-shadow:0 -6px 8px rgba(0,0,0,0.4)}.btn[data-direction="left"].is-open{-webkit-transform:rotateY(-180deg);transform:rotateY(-180deg);box-shadow:0 6px 8px rgba(0,0,0,0.4)}';
+    var css = '.flipbox{display:block;position:relative;border:double 8px red;min-height:100px;-webkit-transition:width .8s cubic-bezier(0.23,1,0.32,1),height .8s cubic-bezier(0.23,1,0.32,1),-webkit-transform .8s cubic-bezier(0.175,0.885,0.32,1.275);transition:width .8s cubic-bezier(0.23,1,0.32,1),height .8s cubic-bezier(0.23,1,0.32,1),transform .8s cubic-bezier(0.175,0.885,0.32,1.275);-webkit-transform-style:preserve-3d;transform-style:preserve-3d;-webkit-transform-origin:50% 50%;-ms-transform-origin:50% 50%;transform-origin:50% 50%}.flipbox-front{z-index:999;position:absolute;display:block;background-color:#fff;width:100%;height:100%;-webkit-backface-visibility:hidden;backface-visibility:hidden;-webkit-tap-highlight-color:transparent;-webkit-transition:background .15s ease,line-height .8s cubic-bezier(0.23,1,0.32,1);transition:background .15s ease,line-height .8s cubic-bezier(0.23,1,0.32,1)}.flipbox-front:hover{background-color:#f77066}.not-allowed .flipbox-front{cursor:pointer}.flipbox-back{z-index:0;position:absolute;width:100%;height:100%;background-color:#eee;color:#222;-webkit-transform:translateZ(-2px) rotateX(180deg);transform:translateZ(-2px) rotateX(180deg);overflow:hidden;-webkit-transition:box-shadow .8s ease;transition:box-shadow .8s ease;text-align:center}.flipbox-back p{margin-top:15px;margin-bottom:10px}.flipbox-back button{padding:12px 20px;width:30%;margin:0 5px;background-color:transparent;border:0;border-radius:2px;font-size:1em;cursor:pointer;-webkit-appearance:none;-webkit-tap-highlight-color:transparent;-webkit-transition:background .15s ease;transition:background .15s ease}.flipbox-back button:focus{outline:0}.flipbox-back button.yes{background-color:#2196f3;color:#fff}.flipbox-back button.yes:hover{background-color:red}.flipbox-back button.no{color:#2196f3}.flipbox-back button.no:hover{background-color:#ddd}.flipbox[data-direction="left"] .flipbox-back,.flipbox[data-direction="right"] .flipbox-back{-webkit-transform:translateZ(-2px) rotateY(180deg);transform:translateZ(-2px) rotateY(180deg)}.flipbox.is-open{border:solid 8px #eee}.flipbox.is-open .flipbox-front{pointer-events:none;z-index:0}.flipbox.is-open .flipbox-back{z-index:999}.flipbox[data-direction="top"].is-open{-webkit-transform:rotateX(180deg);transform:rotateX(180deg);box-shadow:0 -6px 8px rgba(0,0,0,0.4)}.flipbox[data-direction="right"].is-open{-webkit-transform:rotateY(180deg);transform:rotateY(180deg);box-shadow:0 6px 8px rgba(0,0,0,0.4)}.flipbox[data-direction="bottom"].is-open{-webkit-transform:rotateX(-180deg);transform:rotateX(-180deg);box-shadow:0 -6px 8px rgba(0,0,0,0.4)}.flipbox[data-direction="left"].is-open{-webkit-transform:rotateY(-180deg);transform:rotateY(-180deg);box-shadow:0 6px 8px rgba(0,0,0,0.4)}';
 
-    function assemble_flipbox(el) {
+    function assembleFlipbox(el) {
         var h = $(el).outerHeight() + 8;
         var a = $(el).html();
-        a = '<div class="btn-front">' + a + '</div>';
+        a = '<div class="flipbox-front">' + a + '</div>';
         var hb = '';
         if (h > 100) hb = ' style="margin-top:' + ((h-100)/2 + 20) + 'px;"';
-        var b = '<div class="btn-back"><p'+ hb + '>这是一个推广链接，你确定要浏览它吗？</p><button class="yes">确定</button><button class="no">取消</button></div>';
-        $(el).css({"height": h+"px", "min-height": "100px"}).addClass("btn not-allowed").html(b + a);
+        var b = '<div class="flipbox-back"><p'+ hb + '>这是一个推广链接，你确定要浏览它吗？</p><button class="yes">确定</button><button class="no">取消</button></div>';
+        $(el).css("height", h+"px").addClass("flipbox not-allowed").html(b + a);
     }
 
     function hlpl() {
@@ -29,17 +29,17 @@
             if (typeof c != 'undefined') {
                 if (typeof c.fm != 'undefined') {
                     if (c.fm === "pp" || c.fm === "alop") { // 判定是否为百度推广链接 new-pp
-                        assemble_flipbox($(this));
+                        assembleFlipbox($(this));
                     }
-                } else if (typeof c.rsv_cd != 'undefined') {
-                    if (/vLevel:\d/.test(c.rsv_cd)) assemble_flipbox($(this));
+                } else if (typeof c.rsv_cd != 'undefined') { // 判定是否含有V徽章
+                    if (/vLevel:\d/.test(c.rsv_cd)) assembleFlipbox($(this));
                 }
             } else {
                 $(this).children("div").each(function() {
                     var d = $(this).data("click");
                     if (typeof d != 'undefined' && typeof d.fm != 'undefined') {
                         if (d.fm === "ppim") { // 判定是否为百度推广链接 new-ppim
-                            assemble_flipbox($(this));
+                            assembleFlipbox($(this));
                         }
                     }
                 });
@@ -48,21 +48,21 @@
         $("#content_left").prepend('<div id="ajaxFreshed"></div>');
     }
 
-    function bindBtnEvent() {
-        $('.btn').each(function() {
-            var btn = this;
-            var btnFront = btn.querySelector('.btn-front'),
-                btnYes = btn.querySelector('.btn-back .yes'),
-                btnNo = btn.querySelector('.btn-back .no');
+    function bindFlipboxEvent() {
+        $('.flipbox').each(function() {
+            var flipbox = this;
+            var flipboxFront = flipbox.querySelector('.flipbox-front'),
+                btnYes = flipbox.querySelector('.flipbox-back .yes'),
+                btnNo = flipbox.querySelector('.flipbox-back .no');
 
-            btnFront.addEventListener('click', function(event) {
-                var notAllowed = $(this).closest(".btn").hasClass("not-allowed");
+            flipboxFront.addEventListener('click', function(event) {
+                var notAllowed = $(this).closest(".flipbox").hasClass("not-allowed");
                 if (notAllowed) {
-                    var mx = event.clientX - btn.offsetLeft,
-                        my = event.clientY - btn.offsetTop;
+                    var mx = event.clientX - flipbox.offsetLeft,
+                        my = event.clientY - flipbox.offsetTop;
 
-                    var w = btn.offsetWidth,
-                        h = btn.offsetHeight;
+                    var w = flipbox.offsetWidth,
+                        h = flipbox.offsetHeight;
 
                     var directions = [
                         {id: 'top', x: w/2, y: 0},
@@ -74,17 +74,17 @@
                     directions.sort(function(a, b) {
                         return distance(mx, my, a.x, a.y) - distance(mx, my, b.x, b.y);
                     });
-                    btn.setAttribute('data-direction', directions.shift().id);
-                    btn.classList.add('is-open');
+                    flipbox.setAttribute('data-direction', directions.shift().id);
+                    flipbox.classList.add('is-open');
                 }
             } );
 
             btnYes.addEventListener( 'click', function(event) {
-                btn.classList.remove('not-allowed');
-                btn.classList.remove('is-open');
+                flipbox.classList.remove('not-allowed');
+                flipbox.classList.remove('is-open');
             } );
             btnNo.addEventListener('click', function(event){
-                btn.classList.remove('is-open');
+                flipbox.classList.remove('is-open');
             });
         });
     }
@@ -97,16 +97,16 @@
 
     $(document).ajaxComplete(function(event, request, settings) {
         if (!document.getElementById("ajaxFreshed")) {
-            $("<style>").attr("id", "external-style").text(css).appendTo("head");
+            $("<style>").text(css).appendTo("head");
             hlpl();
-            bindBtnEvent();
+            bindFlipboxEvent();
         }
     });
 
     $(document).ready(function() {
-        $("<style>").attr("id", "external-style").text(css).appendTo("head");
+        $("<style>").text(css).appendTo("head");
         hlpl();
-        bindBtnEvent();
+        bindFlipboxEvent();
 
         $('div').on("click", ".not-allowed", function(event){
             event.preventDefault();
