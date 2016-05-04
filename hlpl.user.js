95af86e9605bc261ec545257a5b94400dd7662ed
Chu Xiaonan <chuxiaonan@gmail.com>
2016-05-04 17:15:40 +0800

    update hlpl.user.js
    修改ajax listener使其对ajax请求更加智能化；扩大了“高亮链接”的识别范围。

--- hlpl.user.js
+++ hlpl.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         hlpl.user.js
 // @namespace    https://raw.githubusercontent.com/chuxiaonan/Highlight-promotion-links-of-Baidu-search-results/master/hlpl.user.js
-// @version      0.3
+// @version      0.4
 // @description  Highlight promotion links of Baidu search results
 // @author       Chu Xiaonan
 // @match        https://www.baidu.com/*
@@ -12,13 +12,9 @@
     'use strict';
 
     var css = '.btn{display:block;position:relative;border:double 8px red;-webkit-transition:width .8s cubic-bezier(0.23,1,0.32,1),height .8s cubic-bezier(0.23,1,0.32,1),-webkit-transform .8s cubic-bezier(0.175,0.885,0.32,1.275);transition:width .8s cubic-bezier(0.23,1,0.32,1),height .8s cubic-bezier(0.23,1,0.32,1),transform .8s cubic-bezier(0.175,0.885,0.32,1.275);-webkit-transform-style:preserve-3d;transform-style:preserve-3d;-webkit-transform-origin:50% 50%;-ms-transform-origin:50% 50%;transform-origin:50% 50%}.btn-front{z-index:999;position:absolute;display:block;background-color:#fff;width:100%;height:100%;-webkit-backface-visibility:hidden;backface-visibility:hidden;-webkit-tap-highlight-color:transparent;-webkit-transition:background .15s ease,line-height .8s cubic-bezier(0.23,1,0.32,1);transition:background .15s ease,line-height .8s cubic-bezier(0.23,1,0.32,1)}.btn-front:hover{background-color:#f77066}.not-allowed .btn-front{cursor:pointer}.btn-back{z-index:0;position:absolute;width:100%;height:100%;background-color:#eee;color:#222;-webkit-transform:translateZ(-2px) rotateX(180deg);transform:translateZ(-2px) rotateX(180deg);overflow:hidden;-webkit-transition:box-shadow .8s ease;transition:box-shadow .8s ease;text-align:center}.btn-back p{margin-top:15px;margin-bottom:10px}.btn-back button{padding:12px 20px;width:30%;margin:0 5px;background-color:transparent;border:0;border-radius:2px;font-size:1em;cursor:pointer;-webkit-appearance:none;-webkit-tap-highlight-color:transparent;-webkit-transition:background .15s ease;transition:background .15s ease}.btn-back button:focus{outline:0}.btn-back button.yes{background-color:#2196f3;color:#fff}.btn-back button.yes:hover{background-color:red}.btn-back button.no{color:#2196f3}.btn-back button.no:hover{background-color:#ddd}.btn[data-direction="left"] .btn-back,.btn[data-direction="right"] .btn-back{-webkit-transform:translateZ(-2px) rotateY(180deg);transform:translateZ(-2px) rotateY(180deg)}.btn.is-open{border:solid 8px #eee}.btn.is-open .btn-front{pointer-events:none;z-index:0}.btn.is-open .btn-back{z-index:999}.btn[data-direction="top"].is-open{-webkit-transform:rotateX(180deg);transform:rotateX(180deg);box-shadow:0 -6px 8px rgba(0,0,0,0.4)}.btn[data-direction="right"].is-open{-webkit-transform:rotateY(180deg);transform:rotateY(180deg);box-shadow:0 6px 8px rgba(0,0,0,0.4)}.btn[data-direction="bottom"].is-open{-webkit-transform:rotateX(-180deg);transform:rotateX(-180deg);box-shadow:0 -6px 8px rgba(0,0,0,0.4)}.btn[data-direction="left"].is-open{-webkit-transform:rotateY(-180deg);transform:rotateY(-180deg);box-shadow:0 6px 8px rgba(0,0,0,0.4)}';
-    
+
     function assemble_flipbox(el) {
-        var h = 0;
-        $(el).children().each(function(){
-            h += $(this).outerHeight();
-        });
-        h += 8;
+        var h = $(el).outerHeight() + 8;
         var a = $(el).html();
         a = '<div class="btn-front">' + a + '</div>';
         var hb = '';
@@ -28,16 +24,18 @@
     }
 
     function hlpl() {
-        $("#content_left").children().each(function(){
+        $("#content_left").children().each(function() {
             var c = $(this).data("click");
             if (typeof c != 'undefined') {
                 if (typeof c.fm != 'undefined') {
-                    if (c.fm === "pp") { // 判定是否为百度推广链接 new-pp
+                    if (c.fm === "pp" || c.fm === "alop") { // 判定是否为百度推广链接 new-pp
                         assemble_flipbox($(this));
                     }
+                } else if (typeof c.rsv_cd != 'undefined') {
+                    if (/vLevel:\d/.test(c.rsv_cd)) assemble_flipbox($(this));
                 }
             } else {
-                $(this).children("div").each(function(){
+                $(this).children("div").each(function() {
                     var d = $(this).data("click");
                     if (typeof d != 'undefined' && typeof d.fm != 'undefined') {
                         if (d.fm === "ppim") { // 判定是否为百度推广链接 new-ppim
@@ -47,16 +45,17 @@
                 });
             }
         });
+        $("#content_left").prepend('<div id="ajaxFreshed"></div>');
     }
 
     function bindBtnEvent() {
-        $('.btn').each(function(){
+        $('.btn').each(function() {
             var btn = this;
-            var btnFront = btn.querySelector( '.btn-front' ),
-                btnYes = btn.querySelector( '.btn-back .yes' ),
-                btnNo = btn.querySelector( '.btn-back .no' );
+            var btnFront = btn.querySelector('.btn-front'),
+                btnYes = btn.querySelector('.btn-back .yes'),
+                btnNo = btn.querySelector('.btn-back .no');
 
-            btnFront.addEventListener( 'click', function( event ) {
+            btnFront.addEventListener('click', function(event) {
                 var notAllowed = $(this).closest(".btn").hasClass("not-allowed");
                 if (notAllowed) {
                     var mx = event.clientX - btn.offsetLeft,
@@ -66,49 +65,45 @@
                         h = btn.offsetHeight;
 
                     var directions = [
-                        { id: 'top', x: w/2, y: 0 },
-                        { id: 'right', x: w, y: h/2 },
-                        { id: 'bottom', x: w/2, y: h },
-                        { id: 'left', x: 0, y: h/2 }
+                        {id: 'top', x: w/2, y: 0},
+                        {id: 'right', x: w, y: h/2},
+                        {id: 'bottom', x: w/2, y: h},
+                        {id: 'left', x: 0, y: h/2},
                     ];
 
-                    directions.sort( function( a, b ) {
-                        return distance( mx, my, a.x, a.y ) - distance( mx, my, b.x, b.y );
-                    } );
-
-                    btn.setAttribute( 'data-direction', directions.shift().id );
-                    btn.classList.add( 'is-open' );
+                    directions.sort(function(a, b) {
+                        return distance(mx, my, a.x, a.y) - distance(mx, my, b.x, b.y);
+                    });
+                    btn.setAttribute('data-direction', directions.shift().id);
+                    btn.classList.add('is-open');
                 }
             } );
 
-            btnYes.addEventListener( 'click', function( event ) {
-                btn.classList.remove( 'not-allowed' );
-                btn.classList.remove( 'is-open' );
+            btnYes.addEventListener( 'click', function(event) {
+                btn.classList.remove('not-allowed');
+                btn.classList.remove('is-open');
             } );
-
-            btnNo.addEventListener( 'click', function( event ) {
-                btn.classList.remove( 'is-open' );
-            } );
+            btnNo.addEventListener('click', function(event){
+                btn.classList.remove('is-open');
+            });
         });
     }
 
-    function distance( x1, y1, x2, y2 ) {
+    function distance(x1, y1, x2, y2) {
         var dx = x1-x2;
         var dy = y1-y2;
-        return Math.sqrt( dx*dx + dy*dy );
+        return Math.sqrt(dx*dx + dy*dy);
     }
 
-    $(document).ajaxComplete(function(event,request,settings){
-        if (typeof settings.__partsIndex != 'undefined') {
-            if (request.status == 200) {
-                $("<style>").attr("id", "external-style").text(css).appendTo("head");
-                hlpl();
-                bindBtnEvent();
-            }
+    $(document).ajaxComplete(function(event, request, settings) {
+        if (!document.getElementById("ajaxFreshed")) {
+            $("<style>").attr("id", "external-style").text(css).appendTo("head");
+            hlpl();
+            bindBtnEvent();
         }
     });
 
-    $(document).ready(function(){
+    $(document).ready(function() {
         $("<style>").attr("id", "external-style").text(css).appendTo("head");
         hlpl();
         bindBtnEvent();
